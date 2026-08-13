db8e853ce1c4fe4e5cf525683c845895f75e2a90
kali <kali@kali.kali>
2026-08-13 00:48:57 -0400

    توجيه الصفحة الرئيسية إلى لوحة التحكم

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import include, path
 
 urlpatterns = [
+    path('', lambda request: redirect('admin/')),  # توجيه إلى لوحة التحكم
     path('admin/', admin.site.urls),
     path('api/v1/', include('catalog.urls')),
 ]
